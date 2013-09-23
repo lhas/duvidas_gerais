40d68ce60644d69628527d226463ede32247165d
luizhrqas <luizhrqas@gmail.com>
2013-09-23 00:12:22 -0300

    First commit (Rails 4).

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DuvidasGerais::Application.config.secret_key_base = '6219b85dd850f34db859768b9962b3d63cc2efbdd58038d8f44fdc575a3655baf4d3b2a48c03fb993d6f5f4529858294e0d1779090eb4cb76f5a212d59f9d70d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DuvidasGerais::Application.config.session_store :cookie_store, key: '_duvidas_gerais_session'
